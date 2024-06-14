236569ff1171bddd26ac117c0a9d979ded39ef54
Stephen M <stephen.mdga@gmail.com>
2024-06-13 23:12:55 -0400

    Missing domain files

---
+++ src/domain/users/users.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './controllers';
+import { UsersService } from './services';
+
+@Module({ controllers: [UsersController], providers: [UsersService] })
+export class UsersModule {}
