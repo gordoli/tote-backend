3ac7f1a2c401cbbd0db6a50e54ec43d9de1c717a
Peter Kos <pkos91@icloud.com>
2024-07-31 16:26:26 -0700

    Run linter

--- src/database/migrations/dev/1719557069397-notification-read-status.ts
+++ src/database/migrations/dev/1719557069397-notification-read-status.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NotificationReadStatus1719557069397 implements MigrationInterface {
-    name = 'NotificationReadStatus1719557069397'
+  name = 'NotificationReadStatus1719557069397';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Notifications" ADD "readStatus" boolean NOT NULL DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "Notifications" ADD "readStatus" boolean NOT NULL DEFAULT false`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Notifications" DROP COLUMN "readStatus"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "Notifications" DROP COLUMN "readStatus"`,
+    );
+  }
 }
