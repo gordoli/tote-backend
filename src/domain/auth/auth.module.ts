97dfec54085e16416f8b97bcc56c47224ebb1dd2
Peter Kos <pkos91@icloud.com>
2024-08-02 14:34:55 -0700

    Remove extra jwt import

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { Global, Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AuthController } from './controllers';
 import { OtpService, AuthUserService, UserSocialService } from './services';
 
 @Global()
 @Module({
-  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [JwtService, AuthUserService, UserSocialService, OtpService],
-  exports: [JwtService, AuthUserService, OtpService, UserSocialService],
+  providers: [AuthUserService, UserSocialService, OtpService],
+  exports: [AuthUserService, OtpService, UserSocialService],
 })
 export class AuthModule {}
