15a2b0c7ac5284cf8f085195e064d98b15090a52
Gordon Li <gordonli@Gordons-MacBook-Pro-2.local>
2024-08-07 17:43:15 -0700

    TODO: CHANGED EXPIRE TOKEN TIME TO UNLIMITED

--- src/constants/jwt.constant.ts
+++ src/constants/jwt.constant.ts
@@ -3,6 +3,11 @@ export const JWT_CONSTANT = {
     USER_TOKEN: 'jwt_user_token',
     REFRESH_USER_TOKEN: 'jwt_refresh_user_token',
   },
-  EXPIRE_SECONDS: 10 * 60 * 60,
-  ACCESS_TOKEN_EXPIRE: 5 * 60,
+
+  //TODO: Change this to 5 minutes
+  EXPIRE_SECONDS: 10000000 * 60 * 60,
+  ACCESS_TOKEN_EXPIRE: 100000000 * 60,
+
+  // EXPIRE_SECONDS: 10 * 60 * 60,
+  // ACCESS_TOKEN_EXPIRE: 5 * 60,
 };
